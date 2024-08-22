d822e841fd46a5e4cdf48c8d4705151bea7631fc
LuisFelipeMarrote <luisfelipe.marrote@gmail.com>
2024-08-22 19:05:00 -0300

    Fiz o insere_lista

--- Lexical/definicoes.h
+++ Lexical/definicoes.h
@@ -3,7 +3,9 @@
 #include <string.h> 
 #include <ctype.h> 
 
-enum tipos {smais, smenos, smul, snum, satribuicao, sdoispontos};  
+enum tipos {smais, smenos, smul, snum, satribuicao, sdoispontos, svirgula, 
+            sabre_parenteses, sfecha_parenteses, sponto_virgula, sfim, 
+            };  
 
 typedef struct{ 
   char lexema;          // ver ponteiro / string 
