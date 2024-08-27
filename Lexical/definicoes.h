d932ce2b2885f356542c352774ee7c3867c70e31
LuisFelipeMarrote <luisfelipe.marrote@gmail.com>
2024-08-27 17:22:29 -0300

    Lexema resolvido e lista feita

--- Lexical/definicoes.h
+++ Lexical/definicoes.h
@@ -3,13 +3,15 @@
 #include <string.h> 
 #include <ctype.h> 
 
+#define lexema_size_max 40
 
 enum tipos {smais, smenos, smul, snum, satribuicao, sdoispontos,svirgula, 
             sabre_parenteses, sfecha_parenteses, sponto_virgula, sprograma, sse, sentao,
             ssenao, senquanto, sfaca, sinício, sfim, sescreva, sleia, svar, sinteiro, sbooleano, 
-            sverdadeiro, sfalso, sprocedimento, sfuncao, sdiv, se, sou, snao, sidentificador};  
+            sverdadeiro, sfalso, sprocedimento, sfuncao, sdiv, se, sou, snao, sidentificador, 
+            sdif, smenorig, smenor, smaiorig, smaior};  
 
 typedef struct{ 
-  char* lexema;          // ver ponteiro / string 
+  char lexema[lexema_size_max];          // ver ponteiro / string 
   enum tipos simbolo; 
 }token;
--- Lexical/funcs_iniciais.c
+++ Lexical/funcs_iniciais.c
@@ -18,6 +18,14 @@ Faltas atuais:
 #include <stdlib.h>
 #include "definicoes.h"
 
+
+typedef struct Node Node;
+
+struct Node{ 
+  token tk;          // ver ponteiro / string 
+  Node *next;
+}; 
+
 char caractere;
 char *filename; 
 FILE *fp; 
@@ -28,74 +36,48 @@ void ler()
 { 
     caractere = fgetc(fp); 
 } 
-
-typedef struct{ 
-  token tk;          // ver ponteiro / string 
-  Node *next;
-} Node; 
-
-//falta:
-void pega_token(token *tk){ 
-    /*falta:  
-        - caractere é letra (se atentar a exceções como 'ç') 
-        - erro (descobrir o que retornar) 
-    */  
-    if(47 < caractere && caractere < 58){ 
-        trata_digito(tk);  
-    }else{ 
-        if(/*caractere é letra*/1){ 
-            trata_ident_reserv(tk); 
-        }else{ 
-            if(caractere == ':'){ 
-                trata_atribuicao(tk); 
-            }else{ 
-                if(caractere == '+' || caractere == '-' || caractere == '*'){ 
-                    trata_aritmetico(tk); 
-                }else{ 
-                    if(caractere == '!' || caractere == '<' || caractere == '>' || caractere == '='){ 
-                        trata_relacional(tk); 
-                    }else{ 
-                        if(caractere == ';' || caractere == ','|| caractere == '('|| caractere == ')'|| caractere == '.'){ 
-                            trata_pontuacao(tk); 
-                        }else{ 
-                            /*erro*/ 
-                        } 
-                    } 
-                } 
-            } 
-        } 
-    }
+/// Cria um token baseado num array de char passado
+void set_token_s(token *tk, char string[])
+{ 
+    memcpy ( tk->lexema, string, strlen(string)+1 ); 
 } 
+
+/// Cria um token baseado no caractere atual
+void set_token_c(token *tk)
+{ 
+    memcpy ( tk->lexema, &caractere, sizeof(caractere) ); 
+} 
+
 
 //falta
 void trata_digito(token *tk){ 
     /* Falta:
         confirmar concatenação de caracteres
     */
-    char num = caractere; 
+    char *num = &caractere; 
     ler(); 
     while(caractere > 47 && caractere < 58){ 
-       num = num + caractere; // ver como fazer isso para string 
+       num = num + caractere; // ver como fazer isso para string
         ler(); 
     } 
-    tk-> simbolo = snum; 
-    tk->lexema = num; 
+    tk->simbolo = snum; 
+
 } 
 
 void trata_atribuicao(token *tk){ 
     ler(); 
     if(caractere == '='){ 
-        tk->lexema = ":="; 
+        set_token_s (tk, ":="); 
         tk->simbolo = satribuicao; 
         ler(); 
     }else{  
-        tk->lexema = ':'; 
+        set_token_s (tk, ":"); 
         tk->simbolo = sdoispontos; 
     } 
 } 
 
 void trata_aritmetico(token *tk){ 
-    tk->lexema = caractere; 
+    set_token_c (tk);  
     switch(caractere){
         case '+': 
             tk->simbolo = smais;
@@ -111,8 +93,8 @@ void trata_aritmetico(token *tk){
 } 
 
 
-void trata_pontuacao(token *tk){ 
-    tk->lexema = caractere;
+void trata_pontuacao(token *tk){
+    set_token_c (tk);  
     switch (caractere)
     {
     case ',':
@@ -138,13 +120,14 @@ void trata_pontuacao(token *tk){
 void insere_lista(token *tk)
 {
     Node no;
-    no.tk = *tk;
+
+    memcpy ( &no.tk, tk, sizeof(token) );
     no.next = NULL;
 
     if(primeiro != NULL)
     {
-        atual->next = no;
-        atual = no;
+        atual->next = &no;
+        atual = &no;
     }
     else
     {
@@ -152,6 +135,137 @@ void insere_lista(token *tk)
         atual = primeiro;
     }
 }
+
+void trata_ident_reserv(token *tk){
+    /*Falta:
+        - caractere/letra/'_'
+        - id = id + caractere
+    */
+    char* id = &caractere;
+    int size = 0;
+    ler();
+    while(1/* caractere é letra ou dígito ou "_' */){
+        size += 1; //rever
+        ler();
+    }
+
+    char caract_aux[size];
+    memmove (caract_aux,id,size);
+    set_token_s(tk, caract_aux);
+
+    if (strcmp(caract_aux, "programa") == 0) {
+        tk->simbolo = sprograma;
+    } else if (strcmp(caract_aux, "se") == 0) {
+        tk->simbolo = sse;
+    }else if (strcmp(caract_aux, "entao") == 0) {
+        tk->simbolo = sentao;
+    }else if (strcmp(caract_aux, "senao") == 0) {
+        tk->simbolo = ssenao;
+    }else if (strcmp(caract_aux, "enquanto") == 0) {
+        tk->simbolo = senquanto;
+    }else if (strcmp(caract_aux, "faca") == 0) {
+        tk->simbolo = sfaca;
+    }else if (strcmp(caract_aux, "início") == 0) {
+        tk->simbolo = sinício;
+    }else if (strcmp(caract_aux, "fim") == 0) {
+        tk->simbolo = sfim;
+    }else if (strcmp(caract_aux, "escreva") == 0) {
+        tk->simbolo = sescreva;
+    }else if (strcmp(caract_aux, "leia") == 0) {
+        tk->simbolo = sleia;
+    }else if (strcmp(caract_aux, "var") == 0) {
+        tk->simbolo = svar;
+    }else if (strcmp(caract_aux, "inteiro") == 0) {
+        tk->simbolo = sinteiro;
+    }else if (strcmp(caract_aux, "booleano") == 0) {
+        tk->simbolo = sbooleano;
+    }else if (strcmp(caract_aux, "verdadeiro") == 0) {
+        tk->simbolo = sverdadeiro;
+    }else if (strcmp(caract_aux, "falso") == 0) {
+        tk->simbolo = sfalso;
+    }else if (strcmp(caract_aux, "procedimento") == 0) {
+        tk->simbolo = sprocedimento;
+    }else if (strcmp(caract_aux, "funcao") == 0) {
+        tk->simbolo = sfuncao;
+    }else if (strcmp(caract_aux, "div") == 0) {
+        tk->simbolo = sdiv;
+    }else if (strcmp(caract_aux, "e") == 0) {
+        tk->simbolo = se;
+    }else if (strcmp(caract_aux, "ou") == 0) {
+        tk->simbolo = sou;
+    }else if (strcmp(caract_aux, "nao") == 0) {
+        tk->simbolo = snao;
+    }else{
+        tk-> simbolo = sidentificador;
+    }
+
+} 
+
+void trata_relacional(token *tk){
+    char switchcaractere = caractere;
+    ler();
+    switch(switchcaractere){
+        case '!':
+            if(caractere == '='){
+                tk->simbolo = sdif;
+                ler();
+            }
+            else{
+                //erro
+            }
+            break;
+        case '<':
+            if(caractere == '='){
+                tk->simbolo = smenorig;
+                ler();
+            }
+            else{
+                tk->simbolo = smenor;
+            }
+            break;
+        case '>':
+            if(caractere == '='){
+                tk->simbolo = smaiorig;
+                ler();
+            }
+            else{
+                tk->simbolo = smaior;
+            }
+            break;
+    }
+}
+
+void pega_token(token *tk){ 
+    /*falta:  
+        - caractere é letra (se atentar a exceções como 'ç') 
+        - erro (descobrir o que retornar) 
+    */  
+    if(47 < caractere && caractere < 58){ 
+        trata_digito(tk);  
+    }else{ 
+        if(/*caractere é letra*/1){ 
+            trata_ident_reserv(tk); 
+        }else{ 
+            if(caractere == ':'){ 
+                trata_atribuicao(tk); 
+            }else{ 
+                if(caractere == '+' || caractere == '-' || caractere == '*'){ 
+                    trata_aritmetico(tk); 
+                }else{ 
+                    if(caractere == '!' || caractere == '<' || caractere == '>' || caractere == '='){ 
+                        //trata_relacional(tk); 
+                    }else{ 
+                        if(caractere == ';' || caractere == ','|| caractere == '('|| caractere == ')'|| caractere == '.'){ 
+                            trata_pontuacao(tk); 
+                        }else{ 
+                            /*erro*/ 
+                        } 
+                    } 
+                } 
+            } 
+        } 
+    }
+} 
 
 void AnalisadorLexicalN1()
 {
@@ -196,65 +310,3 @@ void AnalisadorLexicalN1()
 
     fclose(fp); 
 }
-
-void trata_ident_reserv(token *tk){
-    /*Falta:
-        - caractere/letra/'_'
-        - id = id + caractere
-    */
-    char* id = caractere;
-    ler();
-    while(1/* caractere é letra ou dígito ou "_' */){
-        id = id + caractere; //rever
-        ler();
-    }
-    tk->lexema = id;
-
-    if (strcmp(id, "programa") == 0) {
-        tk->simbolo = sprograma;
-    } else if (strcmp(id, "se") == 0) {
-        tk->simbolo = sse;
-    }else if (strcmp(id, "entao") == 0) {
-        tk->simbolo = sentao;
-    }else if (strcmp(id, "senao") == 0) {
-        tk->simbolo = ssenao;
-    }else if (strcmp(id, "enquanto") == 0) {
-        tk->simbolo = senquanto;
-    }else if (strcmp(id, "faca") == 0) {
-        tk->simbolo = sfaca;
-    }else if (strcmp(id, "início") == 0) {
-        tk->simbolo = sinício;
-    }else if (strcmp(id, "fim") == 0) {
-        tk->simbolo = sfim;
-    }else if (strcmp(id, "escreva") == 0) {
-        tk->simbolo = sescreva;
-    }else if (strcmp(id, "leia") == 0) {
-        tk->simbolo = sleia;
-    }else if (strcmp(id, "var") == 0) {
-        tk->simbolo = svar;
-    }else if (strcmp(id, "inteiro") == 0) {
-        tk->simbolo = sinteiro;
-    }else if (strcmp(id, "booleano") == 0) {
-        tk->simbolo = sbooleano;
-    }else if (strcmp(id, "verdadeiro") == 0) {
-        tk->simbolo = sverdadeiro;
-    }else if (strcmp(id, "falso") == 0) {
-        tk->simbolo = sfalso;
-    }else if (strcmp(id, "procedimento") == 0) {
-        tk->simbolo = sprocedimento;
-    }else if (strcmp(id, "funcao") == 0) {
-        tk->simbolo = sfuncao;
-    }else if (strcmp(id, "div") == 0) {
-        tk->simbolo = sdiv;
-    }else if (strcmp(id, "e") == 0) {
-        tk->simbolo = se;
-    }else if (strcmp(id, "ou") == 0) {
-        tk->simbolo = sou;
-    }else if (strcmp(id, "nao") == 0) {
-        tk->simbolo = snao;
-    }else{
-        tk-> simbolo = sidentificador;
-    }
-
-}
-
