e4f04a2fde066da7388a7885574728a8c43b238b
Lucas Fiuza <lucex109@gmail.com>
2024-10-10 18:41:24 -0300

    correções da tabela de símbolos

--- Compilador/simbolos.h
+++ Compilador/simbolos.h
@@ -6,6 +6,7 @@
 #include "lexico.h"
 #include "semantico.h"
 
+typedef struct entrada_tab_simbolos entrada_tab_simbolos;
 typedef struct entrada_tab_simbolos{
     char nome_ident[lexema_size_max];
     char escopo; //rever tipo
@@ -13,6 +14,12 @@ typedef struct entrada_tab_simbolos{
     //adicionar end memoria (ver qual tipo usar)
     entrada_tab_simbolos* prev;
 }entrada_tab_simbolos;
+
+void popula_entrada(entrada_tab_simbolos* entrada, char string[], char escopo, enum tipos tipo){
+    entrada->escopo = escopo;
+    entrada->tipo = tipo;
+    memcpy(entrada->nome_ident, string, strlen(string) + 1);
+}
 
 //"Classe" da tabela de simbolos começa----------------------------------------------
 typedef struct tabela_de_simbolos{
@@ -32,21 +39,24 @@ void insere_tab_simbolos(struct tabela_de_simbolos* self, entrada_tab_simbolos* 
 }
 
 void pop_tab_simbolos(struct tabela_de_simbolos* self){
-    entrada_tab_simbolos* topo_anterior;
-    topo_anterior = self->topo;
-    self->topo = self->topo->prev;
-    free(topo_anterior);
+    if(self->topo->tipo != sbase){
+        entrada_tab_simbolos* topo_anterior = self->topo;
+        self->topo = topo_anterior->prev;
+        free(topo_anterior);
+    }else{
+        printf("não há mais entradas!\n");
+    }
 }
 
 tabela_de_simbolos* nova_tabela(){
     tabela_de_simbolos* tabela = (tabela_de_simbolos*)malloc(sizeof(tabela_de_simbolos));
-    entrada_tab_simbolos base;
+    entrada_tab_simbolos* base = (entrada_tab_simbolos*) malloc(sizeof(entrada_tab_simbolos));
     
     char* nome_base = "base_da_pilha";
-    base.escopo = '-';
-    memcpy(base.nome_ident, nome_base, strlen(nome_base) + 1);
-    base.tipo = sbase;
-    tabela->topo = &base;
+    base->escopo = '-';
+    memcpy(base->nome_ident, nome_base, strlen(nome_base) + 1);
+    base->tipo = sbase;
+    tabela->topo = base;
 
     tabela->insere = insere_tab_simbolos;
     tabela->pop = pop_tab_simbolos;
