9ddb85e3be939ed756403d30d6a923b05a872682
LuEx10 <lucex109@gmail.com>
2024-11-27 10:23:40 -0300

    início da interface da VM (falta títulos, reposicionar botões e redimensionar ao mudar tamanho da janela)

--- VM/testes_interface/clint.c
+++ VM/testes_interface/clint.c
@@ -1,5 +1,8 @@
 #include <windows.h>
 #include <commctrl.h>
+#include <locale.h>
+#include <commdlg.h> 
+#include <stdio.h>
 
 // Window procedure declaration
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
@@ -9,9 +12,12 @@ HINSTANCE g_hInst;
 HWND g_hListView, g_hMemoryListView, g_hOutputEdit;
 HWND g_hNormalRadio, g_hStepByStepRadio;
 HWND g_hExecuteButton, g_hStopButton;
+HMENU g_hMainMenu;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
     LPSTR lpCmdLine, int nCmdShow) {
+    
+    setlocale(LC_ALL, "pt_BR.UTF-8"); // Configura o idioma para português (Brasil)
     
     g_hInst = hInstance;
     
@@ -21,7 +27,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
     wc.lpszClassName = "VirtualMachineClass";
     RegisterClassEx(&wc);
     
@@ -57,42 +63,122 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     return msg.wParam;
 }
 
+void AddMemoryItem(const char* linha, const char* instrucao, 
+                const char* attr1, const char* attr2, const char* comentario) {
+    LVITEM lvi = {0};
+    lvi.mask = LVIF_TEXT;
+    
+    int itemCount = ListView_GetItemCount(g_hListView);
+    
+    lvi.iItem = itemCount;
+    lvi.iSubItem = 0;
+    lvi.pszText = (char*)linha;
+    ListView_InsertItem(g_hListView, &lvi);
+    
+    ListView_SetItemText(g_hListView, itemCount, 1, (char*)instrucao);
+    ListView_SetItemText(g_hListView, itemCount, 2, (char*)attr1);
+    ListView_SetItemText(g_hListView, itemCount, 3, (char*)attr2);
+    ListView_SetItemText(g_hListView, itemCount, 4, (char*)comentario);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_CREATE:
         {
+            //define os tamanhos em relação à janela
+            RECT parentRect;
+            GetClientRect(hwnd, &parentRect);
+            int parent_height = (parentRect.bottom - parentRect.top);
+            int parent_Width = (parentRect.right - parentRect.left);
+
+            //janela de codigo:
+            int hlistViewWidth = parent_Width * 0.76;
+            int hlistViewHeight = parent_height * 0.7;
+            int hlistviewX = 10;
+            int hlistviewY = 10;
+
+            //janela de memoria:
+            int memoryListWidth = parent_Width * 0.2;
+            int memorylistX = hlistviewX + hlistViewWidth + 14;
+            int memorylistY = hlistviewY;
+
+            //saída de dados:
+            int outputWidth = parent_Width * 0.6;
+            int outputHeight = parent_height * 0.22;
+            int outputX = hlistviewX;
+            int outputY = hlistviewY + hlistViewHeight + 5;
+
+            //botoes 
+                //normalX, normalY, normalWidth, normalHeight,
+            int normalWidth = 100;
+            int normalHeight = 30;
+            int normalX = 320;
+            int normalY = 320;
+            
+            int passoWidth = 100;
+            int passoHeight = 30;
+            int passoX = 320;
+            int passoY = 350;
+            
+            int executarWidth = 100;
+            int executarHeight = 30;
+            int executarX = 440;
+            int executarY = 320;
+            
+            int pararWidth = 100;
+            int pararHeight = 30;
+            int pararX = 440;
+            int pararY = 360;
+
+            // Menu
+            g_hMainMenu = CreateMenu();
+            HMENU hFileMenu = CreatePopupMenu();
+            AppendMenu(hFileMenu, MF_STRING, 1000, "Abrir");
+            AppendMenu(hFileMenu, MF_SEPARATOR, 0, NULL);
+            AppendMenu(hFileMenu, MF_STRING, 1001, "Sair");
+
+            AppendMenu(g_hMainMenu, MF_POPUP, (UINT_PTR)hFileMenu, "Arquivo");
+            
+            SetMenu(hwnd, g_hMainMenu);
+
             // Create main ListView (Code table)
             g_hListView = CreateWindowEx(
                 0,
                 WC_LISTVIEW,
                 "",
-                WS_CHILD | WS_VISIBLE | LVS_REPORT,
-                10, 10, 500, 300,
+                WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_NOSORTHEADER |
+                WS_BORDER | WS_VSCROLL | WS_HSCROLL,  // Adiciona bordas e barras de rolagem
+                hlistviewX, hlistviewY,     // posição x, y
+                hlistViewWidth, hlistViewHeight,   // largura, altura fixa
                 hwnd, NULL, g_hInst, NULL
             );
             
+            RECT rect;
+            GetClientRect(g_hListView, &rect);  // Pega o tamanho total do ListView
+            int totalWidth = rect.right - rect.left;  // Calcula a largura total
+
             // Add columns to main ListView
             LVCOLUMN lvc = {0};
             lvc.mask = LVCF_TEXT | LVCF_WIDTH;
             
             lvc.pszText = "Linha";
-            lvc.cx = 50;
+            lvc.cx = totalWidth * 0.10;
             ListView_InsertColumn(g_hListView, 0, &lvc);
             
             lvc.pszText = "Instrução";
-            lvc.cx = 80;
+            lvc.cx = totalWidth * 0.20;
             ListView_InsertColumn(g_hListView, 1, &lvc);
             
             lvc.pszText = "Atributo 1";
-            lvc.cx = 80;
+            lvc.cx = totalWidth * 0.20;
             ListView_InsertColumn(g_hListView, 2, &lvc);
             
             lvc.pszText = "Atributo 2";
-            lvc.cx = 80;
+            lvc.cx = totalWidth * 0.20;
             ListView_InsertColumn(g_hListView, 3, &lvc);
             
             lvc.pszText = "Comentário";
-            lvc.cx = 200;
+            lvc.cx = totalWidth * 0.31;
             ListView_InsertColumn(g_hListView, 4, &lvc);
             
             // Create Memory ListView
@@ -100,18 +186,23 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 0,
                 WC_LISTVIEW,
                 "",
-                WS_CHILD | WS_VISIBLE | LVS_REPORT,
-                520, 10, 250, 300,
+                WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_NOSORTHEADER |
+                WS_BORDER | WS_VSCROLL | WS_HSCROLL,
+                memorylistX, memorylistY, memoryListWidth, hlistViewHeight,
                 hwnd, NULL, g_hInst, NULL
             );
+            ListView_SetExtendedListViewStyle(g_hMemoryListView, LVS_EX_GRIDLINES);
             
+            GetClientRect(g_hMemoryListView, &rect);  // Pega o tamanho total do ListView
+            totalWidth = rect.right - rect.left;  // Calcula a largura total
+
             // Add columns to Memory ListView
             lvc.pszText = "Endereço";
-            lvc.cx = 100;
+            lvc.cx = totalWidth * 0.50;
             ListView_InsertColumn(g_hMemoryListView, 0, &lvc);
             
             lvc.pszText = "Valor";
-            lvc.cx = 100;
+            lvc.cx = totalWidth * 0.51;
             ListView_InsertColumn(g_hMemoryListView, 1, &lvc);
             
             // Create Output Edit box
@@ -120,7 +211,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 "EDIT",
                 "",
                 WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL,
-                10, 320, 300, 100,
+                outputX, outputY, outputWidth, outputHeight,
                 hwnd, NULL, g_hInst, NULL
             );
             
@@ -130,7 +221,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 "BUTTON",
                 "Normal",
                 WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
-                320, 320, 100, 30,
+                normalX, normalY, normalWidth, normalHeight,
                 hwnd, NULL, g_hInst, NULL
             );
             
@@ -139,7 +230,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 "BUTTON",
                 "Passo a Passo",
                 WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
-                320, 350, 100, 30,
+                passoX, passoY, passoWidth, passoHeight,
                 hwnd, NULL, g_hInst, NULL
             );
             
@@ -149,7 +240,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 "BUTTON",
                 "Executar",
                 WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                440, 320, 100, 30,
+                executarX, executarY, executarWidth, executarHeight,
                 hwnd, NULL, g_hInst, NULL
             );
             
@@ -158,10 +249,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 "BUTTON",
                 "Parar",
                 WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                440, 360, 100, 30,
+                pararX, pararY, pararWidth, pararHeight,
                 hwnd, NULL, g_hInst, NULL
             );
             
+            char str1[12] = " ", str2[12] = " ";
+            sprintf(str1, "%d", parent_height);
+            sprintf(str2, "%d", parent_Width);
+            AddMemoryItem("1", "altura", str1, "largura", str2);
+            AddMemoryItem("2", "teste", "sla", "sl2", "str2");
+
+
             // Set default radio button
             SendMessage(g_hStepByStepRadio, BM_SETCHECK, BST_CHECKED, 0);
             
@@ -170,6 +268,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
         
         case WM_COMMAND:
             // Handle button clicks and other commands here
+            //rever isso tudo! (placeholder)
+            switch (LOWORD(wParam))
+                {
+                    case 1000: // "Abrir"
+                        // Adicione o código para abrir um arquivo aqui
+                        break;
+                    case 1001: // "Sair"
+                        DestroyWindow(hwnd);
+                        break;
+                    // Outros casos de comando aqui
+                }
             break;
             
         case WM_DESTROY:
